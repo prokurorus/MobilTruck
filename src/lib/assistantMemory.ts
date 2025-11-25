aaf9d44912f4d8ad95ffcbca06d0b7e70dd5b762
prokurorus <prokurorus2@gmail.com>
2025-11-25 13:28:06 +0200

    Update assistantMemory.ts

--- src/lib/assistantMemory.ts
+++ src/lib/assistantMemory.ts
@@ -5,14 +5,13 @@ import { initializeApp, getApps } from "firebase/app";
 import { getDatabase, ref, push } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCmTNXdBF7ilzeZVB2VaIt1USIMdXA2src",
-  authDomain: "novaciv-web.firebaseapp.com",
-  databaseURL: "https://novaciv-web-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "novaciv-web",
-  storageBucket: "novaciv-web.firebasestorage.app",
-  messagingSenderId: "884571454196",
-  appId: "1:884571454196:web:282712115c2d480299bca1",
-  measurementId: "G-ZX33H70XCZ"
+  apiKey: "AIzaSyAZgVrz0uREv1o3KFm1g-GvKWSCaSLzv8c",
+  authDomain: "mobiltruck-forum.firebaseapp.com",
+  databaseURL: "https://mobiltruck-forum-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "mobiltruck-forum",
+  storageBucket: "mobiltruck-forum.firebasestorage.app",
+  messagingSenderId: "766370719906",
+  appId: "1:766370719906:web:4657583a90425f9f3c4ffb"
 };
 
 const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
